d57ca697cabcc9708e1fae4e1bf4be2ab7164bb3
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-08-26 11:45:58 +0530

    p1

--- DataBse/Mongodata.js
+++ DataBse/Mongodata.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose"
 export const ConntectDb = () =>{
     mongoose.connect(process.env.MONGO_URL,{
     dbName:"userData",
-}).then(()=>{
-    console.log("DataBase is Connected")
+}).then((c)=>{
+    console.log(`Database is conntected with ${c.connection.host}`)
 }).catch((err)=>{
     console.log(err)
 })
